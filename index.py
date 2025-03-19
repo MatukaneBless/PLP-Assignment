5fdc1c5f9d4e7c67d166f1cbdece07e783ae3629
Matukane Blessing Benjamin <140447361+MatukaneBless@users.noreply.github.com>
2025-03-19 22:42:53 +0200

    Add files via upload

---
+++ index.py
@@ -0,0 +1,12 @@
+A = int(input("Enter number A: "))
+B = int(input("Enter number B: "))
+
+sum_result = A + B
+division_result = A / B
+multiplication_result = A * B
+subtraction_result = A - B
+
+print("Sum:", sum_result)
+print("Division:", division_result)
+print("Multiplication:", multiplication_result)
+print("Subtraction:", subtraction_result)
